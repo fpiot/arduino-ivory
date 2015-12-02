531329db24cc67f423c211d0fdfa1b52ee6eb000
Kiwamu Okabe <kiwamu@debian.or.jp>
2015-12-02 19:04:07 +0900

    Do importProc with BUG

--- demo/01_blink/MainIvory.hs
+++ demo/01_blink/MainIvory.hs
@@ -4,8 +4,19 @@
 import Ivory.Language
 import qualified Ivory.Compile.C.CmdlineFrontend as C (compile)
 
+arduino_header :: String
+arduino_header = "Arduino.h"
+
+c_init :: Def ('[] :-> ())
+c_init = importProc "init" arduino_header
+
+c_delay :: Def ('[Uint32] :-> ())
+c_delay = importProc "delay" arduino_header
+
 ivory_main :: Def ('[] :-> Sint16)
 ivory_main = proc "main" $ body $ do
+  call_ c_init
+  call_ c_delay 500
   ret 0
 
 main_ivory_module :: Module
